e4cc783773ad76f9f6118ad9358e6d554e534f29
Carlos A. <caralla@upv.es>
2024-07-11 12:30:26 +0200

    add custom layout

--- index.php
+++ index.php
@@ -1,9 +1,17 @@
 <?php
-    // Initialize the session
-    session_start();
-
     define('__SHAFI_FOLDER', dirname(__FILE__) . '/');
     define('__SHAFI_INC', __SHAFI_FOLDER . 'inc/');
+    define('__SHAFI_CONF_FOLDER', __SHAFI_FOLDER . 'config/');
+
+    // Get rid of the favicon.ico request for the rest of the application
+    if ($_SERVER['REQUEST_URI'] == '/favicon.ico') {
+        header('Content-Type: image/x-icon');
+        echo file_get_contents(__SHAFI_FOLDER . 'favicon.ico');
+        exit();
+    }
+
+    // Initialize the session
+    session_start();
 
     require_once(__SHAFI_INC . 'i18n.php');
     require_once(__SHAFI_INC . 'debug.php');
@@ -20,6 +28,61 @@
     require_once(__SHAFI_INC . 'list.php');
     require_once(__SHAFI_INC . 'router.php');
     require_once(__SHAFI_INC . 'quota.php');
+
+    // Enable to use a custom layout so that the main divs are not included (from the views)
+    global $CUSTOM_LAYOUT;
+    $CUSTOM_LAYOUT = false;
+
+    function custom_layout() {
+        global $CUSTOM_LAYOUT;
+        $CUSTOM_LAYOUT = true;
+    }
+
+    // Enable to hide the toolbar (from the views)
+    global $SHOW_TOOLBAR;
+    $SHOW_TOOLBAR = true;
+    
+    function hide_toolbar() {
+        global $SHOW_TOOLBAR;
+        $SHOW_TOOLBAR = false;
+    }
+    
+    // A function to show the "official" title of the application, along with the messages that are pending (even from other pages, using the $pagecomm plugin)
+    function show_title() {
+        global $__messages;
+        global $pagecomm;
+        ?>
+            <div class="container text-center">
+        <?php 
+            echo implode(' ', $pagecomm->get_messages(true));
+            echo $__messages; 
+        ?>  
+        <h2>Share Files (ShaFi) <i class="fas fa-share-alt"></i></h2>
+        </div>
+    <?php
+    }
+    
+    // Get a new background image to set it as the background of the page
+    function get_background($force = false) {
+        $_SESSION['bgCounter'] = (($_SESSION['bgCounter']??0) + 1) % __SHAFI_BG_REFRESH_RATE;
+        $bg_url = $_SESSION['bgImage']??null;
+        if (($_SESSION['bgCounter'] == 0) || ($bg_url === null)) {
+            $url_content = file_get_contents(__SHAFI_CONF_FOLDER . "background-list.json");
+            $background_urls = [];
+            if ($url_content !== false) {
+                $background_urls = json_decode($url_content);
+            }
+            $bg_url = $background_urls[array_rand($background_urls)];
+        }
+        $_SESSION['bgImage'] = $bg_url;
+        return $bg_url;
+    }
+
+    // Force the refresh of the background image for the next request
+    function refresh_background() {
+        unset($_SESSION['bgCounter']);
+        unset($_SESSION['bgImage']);
+    }
 
     // TODO:
     // - enable mail sending of links
@@ -58,10 +121,18 @@
     });
 
     $router->add(null, null, 'SHAFI_Op_Get', function($handler) {
-        if ($handler->needs_password)
-            include_once('templates/passwd.php');
-        else
+        if ($handler->needs_password) {
+            if (! $handler->correct_password) {
+                include_once('templates/passwd.php');
+                return;
+            }
+        }
+        if ($handler->show_info)
+            include_once('templates/fileinfo.php');
+        else {
+            // If we get to this point, we should show an error
             include_once('templates/error.php'); 
+        }
     });
 
     $router->add('upload', null, 'SHAFI_Op_UploadChunked', 'templates/error.php');
@@ -98,11 +169,17 @@
 
     $router->exec($token, $op);
 ?>
+<?php 
+    ob_start();
+    $router->view($token, $op);
+    $content = ob_get_clean();
+?>
 <html>
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Share Files (ShaFi)</title>
+        <!--<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">-->
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" crossorigin="anonymous">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.css">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome-animation/0.2.1/font-awesome-animation.min.css">
@@ -110,6 +187,7 @@
         <link rel="stylesheet" href="<?php echo get_root_url() ?>css/ddn-table-smart.css">
         <link rel="stylesheet" href="<?php echo get_root_url() ?>css/shafi.css">
         <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+        <!--<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>-->
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>        
         <script src="https://cdnjs.cloudflare.com/ajax/libs/clipboard.js/2.0.4/clipboard.min.js"></script>
         <script src="<?php echo get_root_url() ?>js/notice.js"></script>
@@ -118,11 +196,28 @@
         <script src="<?php echo get_root_url() ?>js/resumable.js"></script>
         <script src="<?php echo get_root_url() ?>js/resumable-link.js"></script>
         <script src="<?php echo get_root_url() ?>js/shafi.js"></script>
+        <style>
+            body {
+                background-size: cover;
+                background-repeat: no-repeat;
+                background-position: center;
+                background-image: url("<?php echo get_background(); ?>");
+            }
+            .rounded {
+                border-radius: 1rem !important;
+            }
+
+            .bg-white {
+                background-color: white !important;
+                opacity: 0.9;
+            }
+        </style>
     </head>
     <body>	
         <div class="debugbar">
             <?php echo $DEBUG; ?>
         </div>
+        <?php if ($SHOW_TOOLBAR) { ?>
         <div class="toolbar">
             <div class="left"><a href="<?php echo get_root_url() ?>"><i class="fas fa-home"></i><span class="label">ShaFi</span></a></div>
             <div class="right w-25">
@@ -171,23 +266,20 @@
                 ?>
             </div>
         </div>
-        <div class="container h-100">
-            <div class="row h-100">
-                <div class="v-center w-100">
+        <?php } ?>
+        <div class="container h-100 d-flex">
+            <?php if (!$CUSTOM_LAYOUT) { ?>
+                <div class="v-center w-100 bg-white rounded px-3 py-3 rounded rounded-lg shadow-lg">
                     <div class="col-md-6 offset-md-3">
-                        <div class="container text-center">
-                            <?php 
-                                echo implode(' ', $pagecomm->get_messages(true));
-                                echo $__messages; 
-                            ?>
-                            <h2>Share Files (ShaFi) <i class="fas fa-share-alt"></i></h2>
-                        </div>
+                        <?php
+                            show_title();
+                        ?>
                     </div>
-                    <?php 
-                    $router->view($token, $op);   
-                    ?>
+                    <?php echo $content; ?>
                 </div>
-            </div>
+            <?php } else { ?>
+                <?php echo $content; ?>
+            <?php } ?>
         </div>
     </body>
 </html>
