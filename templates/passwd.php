d997ac7b863779ef5735a310d5b2dd7c89f722cd
Carlos A. <caralla@upv.es>
2024-07-11 12:32:56 +0200

    use custom layout

--- templates/passwd.php
+++ templates/passwd.php
@@ -1,11 +1,22 @@
-<div class="col-md-4 offset-md-4 v-center text-center">
-    <i class="bigicon fas fa-lock"></i>
-    <form method="POST">
-        <div class="form-group">
-            <input type="password" class="form-control" id="passwd" name="passwd" placeholder="<?php _e('Password') ?>">
+<?php
+    $c_file = $handler->file;
+    custom_layout();
+    hide_toolbar();
+?>
+<div class="container h-100 w-100 d-flex">
+    <div class="card my-auto ms-1 me-auto w-50 rounded rounded-lg shadow-lg">
+        <div class="card-body text-center">
+            <?php show_title(); ?>
+            <h5 class="card-title mt-5"><?php _e('this file is password protected') ?></h5>
+            <i class="bigicon fas fa-lock my-3"></i>
+            <form method="POST">
+                <div class="form-group">
+                    <input type="password" class="form-control" id="passwd" name="passwd" placeholder="<?php _e('Password') ?>">
+                </div>
+                <div class="container">
+                    <button type="submit" class="btn btn-primary btn-lg" id="download" name="download"><?php _e('Unlock') ?></button>
+                </div>
+            </form>
         </div>
-        <div class="container">
-            <button type="submit" class="btn btn-primary btn-lg" id="download" name="download"><?php _e('Download') ?></button>
-        </div>
-    </form>
+    </div>
 </div>
